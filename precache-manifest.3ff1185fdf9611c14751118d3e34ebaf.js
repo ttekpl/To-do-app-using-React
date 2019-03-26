b3a1053f8c31278cf5d628a34fd9a88d618fc0cf
ttek <dkakol01@gmail.com>
2019-03-26 15:56:42 +0100

    Updates

---
+++ precache-manifest.3ff1185fdf9611c14751118d3e34ebaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54c2cb7b261eb5b54524",
+    "url": "/To-do-app-using-React/./static/js/runtime~main.905e5cf8.js"
+  },
+  {
+    "revision": "7867003070b1c906cbcc",
+    "url": "/To-do-app-using-React/./static/js/main.f219febd.chunk.js"
+  },
+  {
+    "revision": "946507ca19d70d749c94",
+    "url": "/To-do-app-using-React/./static/js/2.40ad7508.chunk.js"
+  },
+  {
+    "revision": "7867003070b1c906cbcc",
+    "url": "/To-do-app-using-React/./static/css/main.79bee9e1.chunk.css"
+  },
+  {
+    "revision": "f4cd895166242c83eae537ac8c094e34",
+    "url": "/To-do-app-using-React/./index.html"
+  }
+];
