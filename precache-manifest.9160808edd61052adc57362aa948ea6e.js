ef98d03081ac13ce08c1fc73a1869d4049788afc
ttek <dkakol01@gmail.com>
2019-03-26 15:57:57 +0100

    Updates

---
+++ precache-manifest.9160808edd61052adc57362aa948ea6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "467c59aa0016372f1821",
+    "url": "/To-do-app-using-React/static/js/runtime~main.641f1139.js"
+  },
+  {
+    "revision": "7867003070b1c906cbcc",
+    "url": "/To-do-app-using-React/static/js/main.f219febd.chunk.js"
+  },
+  {
+    "revision": "946507ca19d70d749c94",
+    "url": "/To-do-app-using-React/static/js/2.40ad7508.chunk.js"
+  },
+  {
+    "revision": "7867003070b1c906cbcc",
+    "url": "/To-do-app-using-React/static/css/main.79bee9e1.chunk.css"
+  },
+  {
+    "revision": "9c1c27a5801275377f03d3eb6d4c7bd9",
+    "url": "/To-do-app-using-React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/To-do-app-using-React/./precache-manifest.3ff1185fdf9611c14751118d3e34ebaf.js"
+  "/To-do-app-using-React/precache-manifest.9160808edd61052adc57362aa948ea6e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/To-do-app-using-React/./index.html", {
+workbox.routing.registerNavigationRoute("/To-do-app-using-React/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
